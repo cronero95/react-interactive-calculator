f9d69de8a2bd8e3a98e2d16d7155ef5fdfdbb66e
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-01-31 15:51:37 -0500

    Add the calculator's buttons.
    Add the buttons to be used (1, 2, 3, 4, 5, 6, 7, 8, 9, 0, +, -, *, /, =, .).
    Modify styles in 'App.css' and 'Button.css' to improve the calculator's appareance.
    Create the 'Screen.js' file but for now it's empty.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import freeCodeCampLogo from './images/freecodecamp-logo.png'
+import Button from './components/Button';
 
 function App() {
   return (
@@ -12,10 +13,30 @@ function App() {
         />
       </div>
       <div className='calculator-wrapper'>
-        <div className='buttons-row'></div>
-        <div className='buttons-row'></div>
-        <div className='buttons-row'></div>
-        <div className='buttons-row'></div>
+        <div className='buttons-row'>
+          <Button>1</Button>
+          <Button>2</Button>
+          <Button>3</Button>
+          <Button>+</Button>
+        </div>
+        <div className='buttons-row'>
+          <Button>4</Button>
+          <Button>5</Button>
+          <Button>6</Button>
+          <Button>-</Button>
+        </div>
+        <div className='buttons-row'>
+          <Button>7</Button>
+          <Button>8</Button>
+          <Button>9</Button>
+          <Button>*</Button>
+        </div>
+        <div className='buttons-row'>
+          <Button>=</Button>
+          <Button>0</Button>
+          <Button>.</Button>
+          <Button>/</Button>
+        </div>
         <div className='buttons-row'></div>
       </div>
     </div>
---
+++ src/components/Screen.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Screen = () => (
+    <div className='input'>
+
+    </div>
+);
+
+export default Screen;
